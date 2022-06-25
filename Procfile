3d75fdf422c882e5d0aca531c2b557c10cd4e2e5
Exestos <denis.saigin2002@gmail.com>
2022-06-25 03:54:39 +0300

    init

--- Procfile
+++ Procfile
@@ -1,4 +1,4 @@
-heroku config:set DISABLE_COLLECTSTATIC=1
 web: gunicorn time_tracking.wsgi
 release: python manage.py makemigrations --noinput
+release: python manage.py collectstatic --noinput
 release: python manage.py migrate --noinput
--- time_tracking/settings.py
+++ time_tracking/settings.py
@@ -1,3 +1,4 @@
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -104,9 +105,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATIC_ROOT = '/home/django/django_project/django_project/static'
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
